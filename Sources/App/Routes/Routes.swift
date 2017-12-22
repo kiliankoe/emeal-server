a0084628cc72bd6d121e878069d75cc491e465d0
Kilian Koeltzsch <me@kilian.io>
2017-12-22 23:16:05 +0100

    remove unecessary /update route

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,10 +2,6 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        get("update") { req in
-            return "true"
-        }
-
         // response to requests to /info domain
         // with a description of the request
         get("info") { req in
