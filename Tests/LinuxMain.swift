8d436fab86eda07d7337204087e4b451495a2f0b
Kilian Koeltzsch <me@kilian.io>
2018-01-05 18:59:32 +0100

    only run scraper tests on linux hosts

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,12 +1,15 @@
 #if os(Linux)
 
 import XCTest
-@testable import AppTests
+// @testable import AppTests
+@testable import ScraperTests
 
 XCTMain([
     // AppTests
-    testCase(PostControllerTests.allTests),
-    testCase(RouteTests.allTests)
+    // testCase(PostControllerTests.allTests),
+    // testCase(RouteTests.allTests)
+    testCase(MenuScraperTests.allTests),
+    testCase(MealDetailScraperTests.allTests),
 ])
 
 #endif
