c87aeaadf2a9a1802ee1e6bfaeb50a313d82e2cd
Kilian Koeltzsch <me@kilian.io>
2018-01-07 20:16:26 +0100

    work around possible stuwe url scheme bug

--- Sources/App/Scraper/MenuScraper.swift
+++ Sources/App/Scraper/MenuScraper.swift
@@ -3,7 +3,18 @@ import SwiftSoup
 
 final class MenuScraper {
     static func menuURL(forWeek week: Week, andDay day: Day) -> URL {
-        return URL(string: "https://www.studentenwerk-dresden.de/mensen/speiseplan/w\(week.rawValue)-d\(day.stuweValue).html")!
+        var weekVal = week.rawValue
+        if day == .sunday && Day.today == .sunday {
+            // There appears to be a bug in the Studentenwerk's server code, where the URL scheme
+            // breaks on sundays. Specifically 'w0-d0' does not reference the current week's sunday
+            // when being accessed on a sunday, but the next one. This might be intended behavior,
+            // seeing how their week starts on sunday, but it makes it impossible to get the current
+            // day's meals using the URL scheme below when being run on a sunday.
+            // That's why this workaround exists ✌️
+            weekVal -= 1
+            // And yes, oddly enough this does seem to work with 0 - 1 = -1.
+        }
+        return URL(string: "https://www.studentenwerk-dresden.de/mensen/speiseplan/w\(weekVal)-d\(day.stuweValue).html")!
     }
 
     static func extractMenus(from doc: Document) -> Elements? {
