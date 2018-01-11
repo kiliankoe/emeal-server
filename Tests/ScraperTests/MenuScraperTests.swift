5fb20e7f51549f12dd008c9f0fad3bb9309527cb
Kilian Koeltzsch <me@kilian.io>
2018-01-11 20:24:48 +0100

    fix tests, whoops

--- Tests/ScraperTests/MenuScraperTests.swift
+++ Tests/ScraperTests/MenuScraperTests.swift
@@ -14,12 +14,12 @@ class MenuScraperTests: XCTestCase {
         let menus = MenuScraper.extractMenus(from: document1)!
             .map(MenuScraper.parseMenu)
 
-        XCTAssertEqual(menus[1].canteen, "Mensa Reichenbachstraße")
-        XCTAssertEqual(menus[4].canteen, "Alte Mensa")
+        XCTAssertEqual(menus[1]!.canteen, "Mensa Reichenbachstraße")
+        XCTAssertEqual(menus[4]!.canteen, "Alte Mensa")
 
-        XCTAssertEqual(menus[1].meals[0], "https://www.studentenwerk-dresden.de/mensen/speiseplan/details-197311.html?pni=1")
-        XCTAssertEqual(menus[1].meals.count, 7)
-        XCTAssertEqual(menus[4].meals.count, 0)
+        XCTAssertEqual(menus[1]!.meals[0].absoluteString, "https://www.studentenwerk-dresden.de/mensen/speiseplan/details-197311.html")
+        XCTAssertEqual(menus[1]!.meals.count, 7)
+        XCTAssertEqual(menus[4]!.meals.count, 0)
     }
 
 }
