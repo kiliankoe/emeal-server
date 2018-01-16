faefe3943a6828f1ff26aa80ba096f15dd62320d
Kilian Koeltzsch <me@kilian.io>
2018-01-16 21:45:02 +0100

    remove unecessary info route

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -2,12 +2,6 @@ import Vapor
 
 extension Droplet {
     func setupRoutes() throws {
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
         try resource("canteens", CanteenController.self)
         try resource("meals", MealController.self)
 
